766e3ed41958b8aa71c06af686ea1d918439ccf9
MoHa270xX <mhmdmedo102030@gmail.com>
2025-08-09 07:08:59 +0300

    add notemodel

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String tittle;
+  final String subtittle;
+  final String date;
+  final int color;
+
+  NoteModel({
+    required this.tittle,
+    required this.subtittle,
+    required this.date,
+    required this.color,
+  });
+}
